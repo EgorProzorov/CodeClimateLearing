f41e97f452b6cec514604372e7f3bf025bc8936e
Egor Prozorov <yegorprozorov@yandex.ru>
2025-03-31 11:51:26 +0300

    add linter

---
+++ README.md
@@ -0,0 +1,1 @@
+https://asciinema.org/connect/adf68228-770d-43cb-96c2-5d1102f2ca8e
--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import math
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main():
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+
+    print("Find the smallest common multiple of given numbers.")
+    play_lcm_game(name)
+
+    print("What number is missing in the progression?")
+    play_geometric_progression_game(name)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def play_lcm_game(name):
+    for _ in range(3):
+        numbers = [random.randint(1, 100) for _ in range(3)]
+        correct_answer = lcm(numbers[0], lcm(numbers[1], numbers[2]))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        print(f"Question: {' '.join(map(str, numbers))}")
+        answer = input("Your answer: ")
+
+        if answer.isdigit() and int(answer) == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
+
+
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
+
+
+def play_geometric_progression_game(name):
+    for _ in range(3):
+        start = random.randint(1, 10)
+        ratio = random.randint(2, 5)
+        length = 10
+        progression = [start * (ratio ** i) for i in range(length)]
+        hidden_index = random.randint(0, length - 1)
+        correct_answer = progression[hidden_index]
+        progression[hidden_index] = ".."
+
+        print(f"Question: {' '.join(map(str, progression))}")
+        answer = input("Your answer: ")
+
+        if answer.isdigit() and int(answer) == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    main()
